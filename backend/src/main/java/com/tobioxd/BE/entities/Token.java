1194b3045dc9091fe552ff6a9388df12d3b77262
Nhat Minh <minhnhat.kd.hungyen@gmail.com>
2024-08-13 15:06:08 +0700

    Update User and Token model

---
+++ backend/src/main/java/com/tobioxd/BE/entities/Token.java
@@ -0,0 +1,49 @@
+package com.tobioxd.BE.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "booking_tokens")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@SuppressWarnings("deprecation")
+public class Token {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GenericGenerator(name = "db-uuid", strategy = "guid")
+    @GeneratedValue(generator = "db-uuid")
+    private String id;
+
+    @Column(name = "token", length = 255)
+    private String token;
+
+    @Column(name = "refresh_token", length = 255)
+    private String refreshToken;
+
+    @Column(name = "token_type", length = 50)
+    private String tokenType;
+
+    @Column(name = "expiration_date")
+    private Date expirationDate;
+
+    @Column(name = "refresh_expiration_date")
+    private Date refreshExpirationDate;
+
+    private boolean revoked;
+    private boolean expired;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
---
+++ backend/src/main/java/com/tobioxd/BE/entities/User.java
@@ -0,0 +1,83 @@
+package com.tobioxd.BE.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "booking_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@SuppressWarnings("deprecation")
+public class User implements UserDetails {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GenericGenerator(name = "db-uuid", strategy = "guid")
+    @GeneratedValue(generator = "db-uuid")
+    private String id;
+
+    @Column(name = "phone_number", length = 255, nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(name = "password", length = 255, nullable = false)
+    private String password;
+
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name = "role", length = 255, nullable = false)
+    private String role;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("ROLE_"+getRole().toUpperCase()));
+        return authorityList;
+    }
+    
+    @Override
+    public String getUsername() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
